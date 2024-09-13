50a3f5a7ef0723c55a771b409196b135747a0063
K3rill <razdor.2019@mail.ru>
2024-09-13 13:03:04 +0300

    handle links

--- src/core/router/router.js
+++ src/core/router/router.js
@@ -7,13 +7,28 @@ export class Router {
 	#currentRout = null
 	#layout = null
 	constructor() {
-
 		this.#handleRouteChange()
+		this.#handleLinks
+	}
+	#handleLinks() {
+		document.addEventListener(event => {
+			const target = event.target.closest('a')
+
+			if (target) {
+				event.preventDefault()
+				this.navigate(target.href)
+			}
+		})
 	}
 	getCurrentPath() {
 		return window.location.pathname
 	}
-
+	navigate(path) {
+		if (path !== this.getCurrentPath()) {
+			window.history.pushState({}, '', path)
+			this.#handleRouteChange()
+		}
+	}
 	#handleRouteChange() {
 		const path = this.getCurrentPath() || '/'
 		let route = this.#routes.find(route => route.path === path)
