5d9eb7813de8d9bf5ee5c3948e7e3e22a84b6179
K3rill <razdor.2019@mail.ru>
2024-09-17 15:06:08 +0300

    header and lib

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,9 +1,5 @@
 import { BaseScreen } from '@/core/component/base-screen.component'
-import { $R } from '@/core/rquery/rquery.lib'
 import renderService from '@/core/services/render.service'
-
-import { Field } from '@/components/ui/field/field.component'
-import { UserItem } from '@/components/ui/user-item/user-item.component'
 
 import styles from './home.module.scss'
 import template from './home.template.html'
@@ -14,24 +10,7 @@ export class Home extends BaseScreen {
 	}
 
 	render() {
-		const element = renderService.htmlToElement(
-			template,
-			[
-				new Field({
-					name: 'mwerg',
-					placeholder: 'Enter email',
-					variant: 'green'
-				}),
-				new UserItem({
-					avatarPath:
-						'https://prisma-blog-ebon.vercel.app/blog/posts/type-safe_js_with_JsDoc.png',
-					name: 'Max'
-				})
-			],
-			styles
-		)
-
-		$R(element).find('h1').css('color', 'green')
+		const element = renderService.htmlToElement(template, [], styles)
 
 		return element
 	}
